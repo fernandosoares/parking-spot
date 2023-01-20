ec5b3bdadae9bb9ef2f5f69cd219408ac6fd6ddd
Fernando Soares, O Dev <fernandosoares.dev@gmail.com>
2023-01-20 16:22:43 -0300

    UTC Dates

---
+++ src/main/java/dev/fernandosoares/parkingspot/configs/DateConfig.java
@@ -0,0 +1,26 @@
+package dev.fernandosoares.parkingspot.configs;
+
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+@Configuration
+public class DateConfig {
+    public static final String FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static LocalDateTimeSerializer SERIALIZER = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(FORMAT));
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        JavaTimeModule module = new JavaTimeModule();
+        module.addSerializer(SERIALIZER);
+        return new ObjectMapper().registerModule(module);
+    }
+
+}
